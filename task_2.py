f81abd058927724fc15abe4161bdc471defe0093
Grisha Lavrov <65803914+GLec1er@users.noreply.github.com>
2023-07-03 22:42:24 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from urllib.parse import quote
 
+app = FastAPI()
+
+@app.post('/encode-url')
+def encode_url(url: str):
+    if not url:
+        return 'Missing URL parameter', 400
+
+    encoded_url = quote(url)
+    return encoded_url
